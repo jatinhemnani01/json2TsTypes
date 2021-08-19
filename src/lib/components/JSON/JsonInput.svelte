68b6bc0c5e23321bb663a658830cc39ac38d94a5
jatinhemnani01 <jatinhemnani01@gmail.com>
2021-08-19 21:33:18 +0530

    Json done

--- src/lib/components/JSON/JsonInput.svelte
+++ src/lib/components/JSON/JsonInput.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+  import { JsonInput } from "$lib/stores/Json";
   import { TextArea } from "carbon-components-svelte";
 </script>
 
 <main>
-  <TextArea placeholder="Enter JSON Here" style="resize: none; height: 60vh;" />
+  <TextArea
+    placeholder="Enter JSON Here"
+    style="resize: none; height: 60vh; outline: none;"
+    bind:value={$JsonInput}
+  />
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,12 +4,18 @@
 
 <script lang="ts">
   import Json from "$lib/components/JSON/Json.svelte";
+  import { JsonInput } from "$lib/stores/Json";
+  import { Button } from "carbon-components-svelte";
 </script>
 
 <main class="body">
   <div class="left-side"><Json /></div>
   <div class="right-side">Flex 2</div>
 </main>
+
+<div class="convert-cont">
+  <Button style="padding: 15px;" kind="danger-tertiary">Convert</Button>
+</div>
 
 <style>
   .body {
@@ -25,5 +31,10 @@
   .right-side {
     flex: 1;
     background-color: darkblue;
+  }
+
+  .convert-cont {
+    display: flex;
+    justify-content: center;
   }
 </style>
