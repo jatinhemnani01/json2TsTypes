e4aa753508bf7cb1e3c03a336f1ca988ad9927a1
jatinhemnani01 <jatinhemnani01@gmail.com>
2021-08-19 23:35:16 +0530

    added github and bmac

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -1,5 +1,5 @@
 <div class="head-cont">
-  <h1>JSON To TypeScript Types</h1>
+  <h1>Convert JSON To TypeScript Types</h1>
 </div>
 
 <style>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,7 +1,7 @@
 <div class="nav">
   <input type="checkbox" id="nav-check" />
   <div class="nav-header">
-    <div class="nav-title">JoGeek</div>
+    <div class="nav-title">JSON2TS</div>
   </div>
   <div class="nav-btn">
     <label for="nav-check">
@@ -11,16 +11,11 @@
     </label>
   </div>
 
-  <div class="nav-links">
+  <div style="margin-right: 40px;" class="nav-links">
     <a href="//github.io/jo_geek" target="_blank">Github</a>
-    <a href="http://stackoverflow.com/users/4084003/" target="_blank"
-      >Stackoverflow</a
+    <a href="https://www.buymeacoffee.com/jatinhemnani01" target="_blank"
+      >Buy Me A Coffee</a
     >
-    <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank"
-      >LinkedIn</a
-    >
-    <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
-    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a>
   </div>
 </div>
 
