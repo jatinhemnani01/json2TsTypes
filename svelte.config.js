4e87eca0dbd7a290d3d1e725a389edbbfba23d13
jatinhemnani01 <jatinhemnani01@gmail.com>
2021-08-20 17:36:01 +0530

    added gh pages config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,25 @@
-import preprocess from "svelte-preprocess";
-import node from "@sveltejs/adapter-static";
+import sveltePreprocess from "svelte-preprocess";
+import static_adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
-
+  preprocess: sveltePreprocess(),
   kit: {
+    // By default, `npm run build` will create a standard Node app.
+    // You can create optimized builds for different platforms by
+    // specifying a different adapter
+    //adapter: node(),
+    adapter: static_adapter(),
+
+    // Comment the paths if wants to run in dev mode.
+    paths: {
+      base: "/svelte-kit-blog-demo",
+      assets: "/svelte-kit-blog-demo",
+    },
+
     // hydrate the <div id="svelte"> element in src/app.html
-    adapter: node(),
     target: "#svelte",
   },
 };
